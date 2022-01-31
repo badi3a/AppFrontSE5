099708ef3028d8d6ef62468139a3dd2a0cbfea1c
badi3a <badiaabouhdid@gmail.com>
2022-01-31 10:03:52 +0100

    code séance 2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
