04a35ef28c70e360bae16ac7bd49e64d9b6d75e8
badi3a <badiaabouhdid@gmail.com>
2022-01-31 11:28:38 +0100

    code séance 2.1

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../core/model/product";
 
 @Component({
   selector: 'app-product-list',
@@ -8,11 +9,36 @@ import { Component, OnInit } from '@angular/core';
 
 export class ProductListComponent implements OnInit {
   title: string;
+  listProduct: Product[];
   constructor() {
   }
 
   ngOnInit(): void {
-    this.title="MyApp Store"
+    this.title="MyApp Store";
+    this.listProduct=[
+      { id: 1,
+        name: 'T-shirt 1',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias cupiditate deserunt dignissimos dolorem.',
+        price: 15,
+        nbrLike: 0,
+        quantity: 10,
+        picture: 'assets/t-shirt2.jpeg'},
+
+      { id: 2,
+        name: 'T-shirt 2',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias cupiditate deserunt dignissimos dolorem.',
+        price: 15,
+        nbrLike: 0,
+        quantity: 10,
+        picture: 'assets/t-shirt1.jpeg'},
+      { id: 3,
+        name: 'T-shirt 3',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias cupiditate deserunt dignissimos dolorem.',
+        price: 15,
+        nbrLike: 0,
+        quantity: 0,
+        picture: 'assets/t-shirt2.jpeg'}
+    ]
   }
 
 }
