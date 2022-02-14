4d9e232c1a6fbeb641652cef1484d1cd9ed978ea
badi3a <badiaabouhdid@gmail.com>
2022-02-14 11:14:11 +0100

    code session 4.1: Input Product - Best Seller

--- src/app/best-seller/best-seller.component.ts
+++ src/app/best-seller/best-seller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../core/model/product";
 
 @Component({
   selector: 'app-best-seller',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./best-seller.component.css']
 })
 export class BestSellerComponent implements OnInit {
-
+  list: Product[];
   constructor() { }
 
   ngOnInit(): void {
+    this.list=[
+      { id: 1,
+        name: 'T-shirt 1',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias cupiditate deserunt dignissimos dolorem.',
+        price: 150,
+        nbrLike: 0,
+        quantity: 10,
+        picture: 'assets/image/t-shirt1.jpg'}
+    ]
   }
 
 }
